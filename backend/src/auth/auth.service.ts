77817b97ac85439600644e0be4626ef8441cbcf1
erkmenberat <beraterkmen.offiziell@gmail.com>
2026-06-02 12:49:52 +0200

    build: Auth Service Core

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+type User = {
+    Name : string, 
+    Email: string, 
+    Password : string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService 
+{
+    constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+    async register(newUser : User): Promise<{ access_token: string }>{
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(newUser.Password, salt);
+
+        const user = await this.prisma.user.create({
+            data: {
+                name: newUser.Name, 
+                email: newUser.Email, 
+                password: hash, 
+            }
+        });
+
+        const payload = { username: newUser.Name };
+        return{
+            access_token: await this.jwt.signAsync(payload),
+        }
+    }
+}
