0992676abc8eb8392fb5b240e55cef5fb87c585b
erkmenberat <beraterkmen.offiziell@gmail.com>
2026-06-09 14:56:57 +0200

    fix: NestJS Port Issue

---
+++ .pnpm-store/v10/projects/6145d27b24c7a88dd72014af1ea518a9/src/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService 
+{
+    constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+    async register(newUser : RegisterDto): Promise<{ access_token: string }>{
+        const exists = await this.prisma.user.findUnique({ where: { email: newUser.email }});
+        if(exists) throw new ConflictException('Sen bizi mal mi Zannettin?');
+
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(newUser.password, salt);
+
+        const user = await this.prisma.user.create({
+            data: {
+                name: newUser.name, 
+                email: newUser.email, 
+                password: hash, 
+            }
+        });
+
+        const payload = { username: newUser.name };
+        return{
+            access_token: await this.jwt.signAsync(payload),
+        }
+    }
+
+    async login( User: LoginDto ): Promise<{ access_token: string }> {
+        const exists = await this.prisma.user.findUnique({ where: { email: User.email }});
+        if(!exists) throw new ConflictException('You have to Create an Account first.');
+    
+        const isMatch = await bcrypt.compare(User.password, exists.password);
+        if (!isMatch) throw new UnauthorizedException('Wrong Password');
+
+        const payload = { sub: exists.id, username: exists.name };
+        return {
+            access_token: await this.jwt.signAsync(payload),
+        };
+  }
+}
