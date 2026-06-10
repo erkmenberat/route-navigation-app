e87f7735f189e0b113faa664d82d371d56b2041a
erkmenberat <beraterkmen.offiziell@gmail.com>
2026-06-10 09:33:29 +0200

    fix: changed URL to Localhost

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL ?? 'http://192.168.56.1:4001',
+  baseURL: process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:4001',
   timeout: 8000,
 });
